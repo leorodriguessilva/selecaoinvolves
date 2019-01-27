465f50b85a91d73b75206ee447c31665b4f826b8
Pichau <Pichau@DESKTOP-HIOS90Q>
2019-01-27 05:54:36 -0200

    Adicionado suporte ao cors para API de 'alertas'

--- desafio-fullstack/src/main/java/com/involves/selecao/AlertaController.java
+++ desafio-fullstack/src/main/java/com/involves/selecao/AlertaController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,10 @@ public class AlertaController {
 	private BuscaAlertasService buscaAlertasService;
 	
 	@Autowired
+	
 	private ProcessadorAlertas processador;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping
     public List<Alerta> alertas() {
 		return buscaAlertasService.buscarTodos();
